d836f20b4f873be8f7db8eb6e74f9cbc21f89610
Bontha Sravan Kumar Reddy <bonthasravan@gmail.com>
2018-12-08 17:55:43 +0530

    Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Chakra
+                    Chakra Template
                 </div>
 
                 <div class="links">
