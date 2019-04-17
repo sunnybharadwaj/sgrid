7d4acdb74c9b1da41860da847e4a5663ef35bf2e
Sunny Bharadwaj <sunny@chakradesign.co>
2019-04-17 20:57:23 +0530

    home page responsive done

--- public/js/app.js
+++ public/js/app.js
@@ -47579,7 +47579,7 @@ if (false) {
 /* 43 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n}\n^\n      Invalid CSS after \"}\": expected \"}\", was \"\"\n      in /Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/resources/sass/_responsive.scss (line 270, column 2)\n    at runLoaders (/Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/node_modules/neo-async/async.js:8077:18)\n    at options.error (/Users/sunny/Desktop/untitled folder 2/work/projects/sgrid/project/sgrid/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 /******/ ]);
--- resources/views/partials/client_list.blade.php
+++ resources/views/partials/client_list.blade.php
@@ -1,9 +1,8 @@
 <div id="client-list">
     <div class="container">
         <div class="text-center">
-
-            <img id="mob-clients" src="{{url('/media/logos/mobile_client_logos@2x.png')}}" alt=""
-                 class="img-fluid">
+            <img id="lg-clients" class="lg-only img-fluid" src="{{url('/media/logos/lg_client_logos@2x.png')}}" alt="">
+            <img id="mob-clients" class="mobile-only img-fluid" src="{{url('/media/logos/mobile_client_logos@2x.png')}}" alt="">
         </div>
     </div>
 </div>
